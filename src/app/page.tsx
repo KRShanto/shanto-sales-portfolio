ce08d286be01d54e57f2694d2ad3f3b2713f7e39
KR Shanto <krshanto2005@gmail.com>
2024-11-16 13:55:42 +0600

    install next-theme and bg dot highligher.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,18 @@
+"use client";
+import { HeroHighlight } from "@/components/ui/bg-highlight";
+
 export default function Page() {
   return (
     <div>
-      <h1>Hello world</h1>
+      <HeroHighlightDemo />
     </div>
+  );
+}
+
+export function HeroHighlightDemo() {
+  return (
+    <HeroHighlight className="h-screen">
+      <h1>Hello world</h1>
+    </HeroHighlight>
   );
 }
