f3539e55e14c7ae6557f9c16fe3b798cd4f02c57
KR Shanto <krshanto2005@gmail.com>
2024-11-16 13:59:28 +0600

    refactor: update ESLint rules and integrate HeroHighlight component in layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
 import { ThemeProvider } from "@/components/theme-provider";
+import { HeroHighlight } from "@/components/ui/bg-highlight";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -29,14 +30,16 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <ThemeProvider
-          attribute="class"
-          defaultTheme="dark"
-          enableSystem
-          disableTransitionOnChange
-        >
-          {children}
-        </ThemeProvider>
+        <HeroHighlight>
+          <ThemeProvider
+            attribute="class"
+            defaultTheme="dark"
+            enableSystem
+            disableTransitionOnChange
+          >
+            {children}
+          </ThemeProvider>
+        </HeroHighlight>
       </body>
     </html>
   );
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,9 @@
 "use client";
-import { HeroHighlight } from "@/components/ui/bg-highlight";
 
 export default function Page() {
   return (
     <div>
-      <HeroHighlightDemo />
+      <h1>Fuck you</h1>
     </div>
-  );
-}
-
-export function HeroHighlightDemo() {
-  return (
-    <HeroHighlight className="h-screen">
-      <h1>Hello world</h1>
-    </HeroHighlight>
   );
 }
